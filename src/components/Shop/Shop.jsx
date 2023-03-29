20eaa448647e1aa16561deca0862d1d64a79c390
abidt20ahmed <abidahmedmahin21@gmail.com>
2023-03-29 06:44:40 +0600

    retrieve product from stored cart and set it to cart state

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
-// import products from '../Product/Product'; '../../fakeData/products.json'
+
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
@@ -11,9 +12,40 @@ const Shop = () => {
         .then(res=> res.json())
         .then(data=> setProducts(data))
     }, [])
+    
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        const savedCart = [];
+        // step 1: get id
+        for(const id in storedCart){
+            // step 2: get the product by using id
+            const addedProduct = products.find(product => product.id === id);
+            
+            // step 3:get quantity of the product
+            if(addedProduct){
+                const quantity = storedCart[id];
+                addedProduct.quantity = quantity;
+                savedCart.push(addedProduct);
+            }
+            console.log(addedProduct);
+        }
+        setCart(savedCart)
+    },[products])
+    
+    
     const handleAddToCart = (product) => {
         const newCart = [...cart, product];
-        setCart(newCart)
+        setCart(newCart);
+        addToDb(product.id)
+        // const storedCarts = getShoppingCart();
+        // console.log(storedCarts);
+        // let sum = 0;
+        // for(let arts in storedCarts){
+        //     const allCarts = storedCarts[arts];
+        //     sum = sum +allCarts;
+        // }
+        // console.log(sum);
+        
     }
     
     return (
@@ -28,7 +60,7 @@ const Shop = () => {
             }
             </div>
             <div>
-           <Cart cart={cart}></Cart>
+            <Cart cart={cart}></Cart>
             </div>
             </div>
             );
